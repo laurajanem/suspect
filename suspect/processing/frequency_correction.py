466dc8010be14543e3541a60292e59fffa9526fb
laurajanem <laura.j.mariano@gmail.com>
2017-03-31 14:59:33 -0400

    Many changes made to this module:
    Added functionality for automatic identification of a target signal to use for registration
    Fixed bug with the way the subset of the frequency range was being computed
    Added functionality for specification of frequency and time domain constraints for registration
    
    Improves performance of the signal alignment significantly

--- suspect/processing/frequency_correction.py
+++ suspect/processing/frequency_correction.py
@@ -13,23 +13,20 @@ def residual_water_alignment(data):
     return peak_index * data.df
 
 
-def spectral_registration(data, target, initial_guess=(0.0, 0.0), frequency_range=None):
+def spectral_registration(data, target, initial_guess=(0.0, 0.0), frequency_range=None, time_range=None):
     """
     Performs the spectral registration method to calculate the frequency and
     phase shifts between the input data and the reference spectrum target. The
     frequency range over which the two spectra are compared can be specified to
     exclude regions where the spectra differ.
-
     Parameters
     ----------
     data : MRSData
     target : MRSData
     initial_guess : tuple
     frequency_range : 
-
     Returns
     -------
-
     """
 
     # make sure that there are no extra dimensions in the data
@@ -42,21 +39,37 @@ def spectral_registration(data, target, initial_guess=(0.0, 0.0), frequency_rang
     # be a numpy.array of the same size as the data in which case we simply use
     # that array as the weightings for the comparison
     if type(frequency_range) is tuple:
-        spectral_weights = frequency_range[0] < data.frequency_axis() & data.frequency_axis() < frequency_range[1]
-    else:
+        spectral_weights = numpy.logical_and(data.frequency_axis() > frequency_range[0],data.frequency_axis() < frequency_range[1])
+    elif frequency_range is not None:
+        # If frequency_range is a numpy array...
         spectral_weights = frequency_range
+    elif frequency_range is None:
+        # Spectral weights  = 1 for all locations
+        spectral_weights = numpy.ones(data.shape)
+    else:
+        # TODO
+        x=1 # Some kind of error message
+    
+    # Apply the spectral weights to the spectrum
+    d = data.spectrum()*spectral_weights
+    t = target.spectrum()*spectral_weights
+    
+    # Convert back to the time-domain MRSData object
+    data = d.fid()
+    target = t.fid()
+    
+    # Check on time_axis constraints
+    if type(time_range) is tuple:
+        # Run the algorithm on a subset of the samples only, defined by the input range
+        time_idx = numpy.logical_and(data.time_axis()>=time_range[0],data.time_axis()<=time_range[1])
+        data = data[time_idx]
+        target = target[time_idx]
+            
 
     # define a residual function for the optimizer to use
     def residual(input_vector):
-        #transformed_data = transform_fid(data, -input_vector[0], -input_vector[1])
         transformed_data = data.adjust_frequency(-input_vector[0]).adjust_phase(-input_vector[1])
         residual_data = transformed_data - target
-        if frequency_range is not None:
-            spectrum = residual_data.spectrum()
-            weighted_spectrum = residual_data * spectral_weights
-            # remove zero-elements
-            weighted_spectrum = weighted_spectrum[weighted_spectrum != 0]
-            residual_data = numpy.fft.ifft(numpy.fft.ifftshift(weighted_spectrum))
         return_vector = numpy.zeros(len(residual_data) * 2)
         return_vector[:len(residual_data)] = residual_data.real
         return_vector[len(residual_data):] = residual_data.imag
@@ -64,3 +77,42 @@ def spectral_registration(data, target, initial_guess=(0.0, 0.0), frequency_rang
 
     out = scipy.optimize.leastsq(residual, initial_guess)
     return out[0][0], out[0][1]
+    
+def select_target_for_spectal_registration(data, frequency_range = None):
+    """
+    Identifies a candidate target signal from the ensemble that can be used for 
+    spectral registration. The candidate is the signal whose maximum abs in the
+    frequency domain is closest to the median maximum abs values of all the 
+    signals in the ensemble.
+    
+    Parameters
+    ----------
+    data : MRSData
+    
+    frequency_range: tuple 
+        Specifies the upper and lower bounds in the frequency domain to use for
+        selecting the target
+    
+    Returns
+    -------
+    target_idx: integer 
+        Index of the selected target signal
+    
+    """
+    if type(frequency_range) is tuple:
+        spectral_weights = numpy.logical_and(data.frequency_axis(),frequency_range[0],data.frequency_axis() < frequency_range[1])
+    else:
+        spectral_weights = frequency_range
+    
+    filtered_spectrum = spectral_weights*data.spectrum()
+    max_vals = numpy.max(numpy.abs(filtered_spectrum),axis=1)
+    target_idx = numpy.argmin(numpy.abs(numpy.median(max_vals)-max_vals))
+
+    return target_idx
+    
+    
+    
+    
+    
+    
+    
